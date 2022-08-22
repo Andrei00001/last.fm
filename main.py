5c1285ea9c8d266e55a14b066544093d72e08aeb
andrei <sherlat01@mail.ru>
2022-08-22 16:12:29 +0300

    update

--- main.py
+++ main.py
@@ -2,11 +2,8 @@ from threading import Thread
 
 from aiogram import Dispatcher
 from aiogram.utils import executor
-import asyncio
-from app.echo import echo
+from app_telegram.echo import echo
 from connection_bot import dp
-from connection_db import Base, engine
-from database.models import User
 
 
 def handlers(dp: Dispatcher):
