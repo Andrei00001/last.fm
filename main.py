baf46adafd0a3f261cabc4d08c6b9ba7466364bb
andrei <sherlat01@mail.ru>
2022-08-22 16:59:24 +0300

    add fastapi

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from threading import Thread
-
 from aiogram import Dispatcher
 from aiogram.utils import executor
+from fastapi import FastAPI
+
 from app_telegram.echo import echo
 from connection_bot import dp
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 
 def handlers(dp: Dispatcher):
@@ -11,8 +18,5 @@ def handlers(dp: Dispatcher):
 
 
 if __name__ == "__main__":
-
     handlers(dp)
     executor.start_polling(dp, skip_updates=True)
-
-
